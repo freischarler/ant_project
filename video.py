7afff2c3a98a927f5270115f2fd629337f6a0b3b
Martin Paz <martin.paz@live.com.ar>
2020-05-21 11:35:30 -0300

    subprocess v2

--- video.py
+++ video.py
@@ -1,65 +1,20 @@
 
+class Video():
+    crop_bool=0
+    modo_fullscreen=1
+    windows_x=0
+    windows_y=0
+    resize=1
+    crop_x=0
+    crop_y=0
+    crop_w=1
+    crop_h=1
+    windows_posx=0
+    windows_posy=0
 
-self.cargar_default()
-        #print(str(self.crop_bool))
-        try:
-            camera=PiCamera()
-            #print("CROP BOOL RECIBIDO"+str(self.crop_bool))
-            if(self.crop_bool==1):
-                print(str(self.crop_x)+str(self.crop_y)+str(self.crop_w)+str(self.crop_h))
-                regionOfInterest = (float(self.crop_x),float(self.crop_y),float(self.crop_w),float(self.crop_h))
-                (roiX, roiY, roiW, roiH) = regionOfInterest
-                
-                width  = int(self.windows_x)*roiW                     #Desired width
-                height = int(self.windows_y)*roiH                    #Desired height
-                
-                percentAspectRatio = roiW/roiH                #Ratio in percent of size 
-                imageAspectRatio   = width/height             #Desired aspect ratio
-                sensorAspectRatio  = int(self.windows_x)/int(self.windows_y) #Physical sensor aspect ratio       
 
-                #The sensor is automatically cropped to fit current aspect ratio 
-                #so we need to adjust zoom to take that into account
-                if (imageAspectRatio > sensorAspectRatio):    
-                    roiY = (roiY - 0.5) * percentAspectRatio + 0.5  
-                    roiH = roiW                              
-        
-                if (imageAspectRatio < sensorAspectRatio):   
-                    roiX = (roiX - 0.5) * percentAspectRatio + 0.5  
-                    roiW = roiH 
-                self.actionactionStop.setEnabled(True)
-                camera.resolution=(int(self.windows_x),int(self.windows_y))
-                
-                camera.zoom=(roiX,roiY,roiW,roiH)
-                camera.start_preview()
-                #camera.start_recording("pythonVideo.h264")
-                time.sleep(5)
-                camera.stop_preview()
-                camera.close()
-            else:
-                if self.modo_fullscreen==0:
-                    print("MODO NO-FULL-SCREEN: "+str(self.windows_x)+" "+str(self.windows_y))
-                    camera.resolution = (int(self.windows_x),int(self.windows_y))
-                    camera.start_preview(fullscreen=False,window=(self.windows_posx,self.windows_posy,int(640/self.resize),int(480/self.resize)))
-                    time.sleep(5)
-                    camera.stop_preview()
-                    camera.close()
-                else:
-                    print("MODO FULL-SCREEN: "+str(self.windows_x)+" "+str(self.windows_y))
-                    camera.resolution = (int(self.windows_x),int(self.windows_y))
-                    camera.start_preview(fullscreen=True)
-                    time.sleep(5)
-                    camera.stop_preview()
-                    camera.close()
-                    
-        except KeyboardInterrupt:
-            print("interrumpiendo")
-            camera.stop_preview()
-            camera.close()
-
-            
-def cargar_default(self):
+    def cargar_default():
         print("LEYENDO VALORES DE LOS TXT")
-        
         try:
             archivo3=open("crop.txt")
             self.crop_x=float(archivo3.readline().replace('\n', ''))
@@ -87,7 +42,7 @@ def cargar_default(self):
                 self.windows_posx=int(archivoRES.readline())
                 self.windows_posy=int(archivoRES.readline())
                 self.resize=int(archivoRES.readline())
-             
+                
             archivoRES.close()
 
             print("LEE GRABACION")
@@ -102,4 +57,70 @@ def cargar_default(self):
             print("VALORES CARGADOS")
         except:
             self.duracion_grabacion=5
-            self.cantidad_videos=1
+            self.cantidad_videos=1    
+    
+def main():
+    newVideo= Video()
+    newVideo.cargar_default
+
+    try:
+        camera=PiCamera()
+            #print("CROP BOOL RECIBIDO"+str(self.crop_bool))
+        if(newVideo.crop_bool==1):
+            print(str(newVideo.crop_x)+str(newVideo.crop_y)+str(newVideo.crop_w)+str(newVideo.crop_h))
+            regionOfInterest = (float(newVideo.crop_x),float(newVideo.crop_y),float(newVideo.crop_w),float(newVideo.crop_h))
+            (roiX, roiY, roiW, roiH) = regionOfInterest
+                    
+            width  = int(newVideo.windows_x)*roiW                     #Desired width
+            height = int(newVideo.windows_y)*roiH                    #Desired height
+                    
+            percentAspectRatio = roiW/roiH                #Ratio in percent of size 
+            imageAspectRatio   = width/height             #Desired aspect ratio
+            sensorAspectRatio  = int(newVideo.windows_x)/int(newVideo.windows_y) #Physical sensor aspect ratio       
+
+                    #The sensor is automatically cropped to fit current aspect ratio 
+                    #so we need to adjust zoom to take that into account
+            if (imageAspectRatio > sensorAspectRatio):    
+                roiY = (roiY - 0.5) * percentAspectRatio + 0.5  
+                roiH = roiW                                  
+            
+            if (imageAspectRatio < sensorAspectRatio):   
+                roiX = (roiX - 0.5) * percentAspectRatio + 0.5  
+                roiW = roiH 
+            
+            newVideo.actionactionStop.setEnabled(True)
+            camera.resolution=(int(newVideo.windows_x),int(newVideo.windows_y))
+                    
+            camera.zoom=(roiX,roiY,roiW,roiH)
+            camera.start_preview()
+                    #camera.start_recording("pythonVideo.h264")
+            time.sleep(5)
+            camera.stop_preview()
+            camera.close()                
+        else:
+            if newVideo.modo_fullscreen==0:
+                print("MODO NO-FULL-SCREEN: "+str(newVideo.windows_x)+" "+str(newVideo.windows_y))
+                camera.resolution = (int(newVideo.windows_x),int(newVideo.windows_y))
+                camera.start_preview(fullscreen=False,window=(newVideo.windows_posx,newVideo.windows_posy,int(640/newVideo.resize),int(480/newVideo.resize)))
+                time.sleep(5)
+                camera.stop_preview()
+                camera.close()        
+            else:
+                print("MODO FULL-SCREEN: "+str(newVideo.windows_x)+" "+str(newVideo.windows_y))
+                camera.resolution = (int(newVideo.windows_x),int(newVideo.windows_y))
+                camera.start_preview(fullscreen=True)
+                time.sleep(5)
+                camera.stop_preview()
+                camera.close()        
+                        
+    except KeyboardInterrupt:
+                print("interrumpiendo")
+                camera.stop_preview()
+                camera.close()                
+
+if __name__ == "__main__":
+    main()
+
+
+
+            
